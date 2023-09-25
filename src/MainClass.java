aca4da5e4cae50d533c85e1d56611895f08f8b5a
ranj1985 <ranjanarupa@gmail.com>
2023-09-25 17:42:02 -0500

    second time commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,14 @@
+import addition.Add;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-
+		Add add=new Add();
+		
+		add.addNumber(10,20);	
+		add.addNumber(5, 6, 7);
+		
 	}
 
 }
--- src/addition/Add.java
+++ src/addition/Add.java
@@ -2,4 +2,15 @@ package addition;
 
 public class Add {
 
+	public void addNumber(int a, int b) {
+		
+	System.out.println(a+b);
+	}
+	
+	public void addNumber(int a, int b, int c ) {
+		
+		System.out.println(a+b+c);
+	}
+	
+
 }
