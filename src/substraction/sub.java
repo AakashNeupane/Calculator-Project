1c665f97124392080097118b39bb0d4814dd6c28
dineshchaudhary <chy.dinesh66@gmail.com>
2023-09-25 20:36:44 -0500

    subtracting something

--- src/substraction/sub.java
+++ src/substraction/sub.java
@@ -2,4 +2,18 @@ package substraction;
 
 public class sub {
 
+	int add (int a, int b) {
+		return a - b;
+	}
+		
+
+		public void addNumber(int a, int b) {
+			
+		System.out.println(a-b);
+		}
+		
+		public void addNumber(int a, int b, int c ) {
+			
+			System.out.println(a-b-c);
+		}
 }
