d7b060d8a704ea2bafa79b8b5f0bb0b941b4709e
Ajit Shrestha <ajitshrestha1516@gmail.com>
2023-09-28 18:31:11 -0500

    changes on new branch

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import addition.Add;
+import division.Div;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+         
+		Add add=new Add();
+		
+		add.addNumber(78,29);	
+	
+		Div div= new Div();
+		div.Divison(10, 02);
+		
+		
+
+	}
+
+}
